bc69e0292a81adf6979612e7f44c356a3d4510c5
Alexander Makarevich <alexander.s.makarevich@gmail.com>
2014-05-25 14:44:49 +0700

    Создаём своё первое приложение с Django, часть 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = ('/home/am/projects/orginfo/mysite/mysite/templates',)
 
 
 # Quick-start development settings - unsuitable for production
