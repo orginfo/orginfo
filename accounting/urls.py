2acc2d75d39b79ac805dd7a5b95c4b2dc0cf8591
Alexander Makarevich <alexander.s.makarevich@gmail.com>
2014-06-05 23:59:56 +0700

    Removed organization_create url.

--- accounting/urls.py
+++ accounting/urls.py
@@ -4,7 +4,6 @@ from accounting import views
 urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounting/login.html'}, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login/'}),
-    #url(r'^organization/create/$', views.create_organization, name='create_organization'),
     url(r'^organization_details/$', views.organization_details, name='organization_details'),
     url(r'^clients/$', views.clients, name='clients'),
     url(r'^$', views.index, name='index'),
