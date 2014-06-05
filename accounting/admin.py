c08a4befafab854f2ead2e14db24078d6e09f0d0
Alexander Makarevich <alexander.s.makarevich@gmail.com>
2014-06-05 23:23:40 +0700

    Do human-readable admin

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from accounting.models import Organization, UserOrganization
- 
-admin.site.register(Organization)
-admin.site.register(UserOrganization)
+from accounting.models import Organization, UserOrganization, Client
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('lfm', 'organization')
+
+class UserOrganizationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'organization')
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(UserOrganization, UserOrganizationAdmin)
+admin.site.register(Client, ClientAdmin)
--- accounting/models.py
+++ accounting/models.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 
 class Organization(models.Model):
     name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
 class UserOrganization(models.Model):
     user = models.ForeignKey(User)
     organization = models.ForeignKey(Organization, null=True, blank=True, default = None)
+
+class Client(models.Model):
+    lfm = models.CharField(max_length=200)
+    organization = models.ForeignKey(Organization)
