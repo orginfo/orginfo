21dd3a89dd74cc941313f210e3c585eff47db947
Alexander Makarevich <alexander.makarevich@eltex.loc>
2014-06-11 18:04:25 +0700

    base

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from accounting.models import Organization, UserOrganization, Client
+from accounting.models import Organization, UserOrganization, Client, Payment
 
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -10,6 +10,10 @@ class ClientAdmin(admin.ModelAdmin):
 class UserOrganizationAdmin(admin.ModelAdmin):
     list_display = ('user', 'organization')
 
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'client')
+
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(UserOrganization, UserOrganizationAdmin)
 admin.site.register(Client, ClientAdmin)
+admin.site.register(Payment, PaymentAdmin)
--- accounting/views.py
+++ accounting/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from accounting.forms import OrganizationForm
-from accounting.models import Organization, UserOrganization, Client
+from accounting.models import Organization, UserOrganization, Client, Payment
+from django.views.generic.edit import CreateView
+
 
 @login_required(login_url="/login/")
 def index(request):
@@ -42,3 +44,22 @@ def clients(request):
     user_org = get_object_or_404(UserOrganization, user=request.user.id)
     clients = Client.objects.filter(organization=user_org.organization_id)
     return render(request, 'accounting/clients.html', {'clients': clients})
+
+class TakePayment(CreateView):
+    model = Payment
+    template_name = 'accounting/take_payment.html'
+    fields = ['amount']
+    exclude = ('client',)
+    def dispatch(self, *args, **kwargs):
+        user_org = get_object_or_404(UserOrganization, user=self.request.user.id)
+        clients = user_org.organization.client_set.all()
+        client_id = int(kwargs['client_id'])
+        self.client = next((client for client in clients if client.id==client_id), None)
+        if not self.client:
+            raise Http404
+        return super(TakePayment, self).dispatch(*args, **kwargs)
+    def get_initial(self):
+        return {'client': 2}
+    def form_valid(self, form):
+         form.instance.client = self.client
+         return super(TakePayment, self).form_valid(form)
