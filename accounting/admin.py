66dc7f0dae74b9ac106a1e66d729e872b304ee3a
Alexander Makarevich <alexander.makarevich@eltex.loc>
2014-07-23 12:01:12 +0700

    Refactored the application data struct.

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from accounting.models import Organization, UserOrganization, Client, RealEstate, Payment
+from accounting.models import Organization, UserOrganization, Client, RealEstate, Payment, ServiceClient, ColdWaterCounter, ColdWaterValue
 
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('lfm', 'organization')
+    list_display = ('lfm', 'organization', 'amount')
 
 class UserOrganizationAdmin(admin.ModelAdmin):
     list_display = ('user', 'organization')
@@ -16,8 +16,20 @@ class RealEstateAdmin(admin.ModelAdmin):
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('amount', 'client', 'real_estate')
 
+class ServiceClientAdmin(admin.ModelAdmin):
+    list_display = ('client', 'service_name')
+
+class ColdWaterCounterAdmin(admin.ModelAdmin):
+    list_display = ('value', 'real_estate', 'date')
+
+class ColdWaterValueAdmin(admin.ModelAdmin):
+    list_display = ('value', 'real_estate', 'date')
+
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(UserOrganization, UserOrganizationAdmin)
 admin.site.register(Client, ClientAdmin)
 admin.site.register(RealEstate, RealEstateAdmin)
 admin.site.register(Payment, PaymentAdmin)
+admin.site.register(ServiceClient, ServiceClientAdmin)
+admin.site.register(ColdWaterCounter, ColdWaterCounterAdmin)
+admin.site.register(ColdWaterValue, ColdWaterValueAdmin)
--- accounting/models.py
+++ accounting/models.py
@@ -2,27 +2,78 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Organization(models.Model):
+    """Организация.
+
+    МУП - это частный случай организации, поэтому МУП находится в этой модели.
+    """
     name = models.CharField(max_length=200)
     def __str__(self):
         return self.name
 
 class UserOrganization(models.Model):
+    """Связь пользователь-организация.
+
+    Пользователи бывают администраторами, клиентами организации, работниками
+    организации. Связь показывает какой именно пользователь сайта является
+    работником организации.
+    """
     user = models.ForeignKey(User)
     organization = models.ForeignKey(Organization, null=True, blank=True, default = None)
 
 class Client(models.Model):
+    """Клиент.
+
+    Клиент принадлежит организации, которая предоставляет ему услуги.
+    У клиента есть свой лицевой счет, который характеризуется его номером,
+    сейчас это ID клиента, и суммой на лицевом счете. Лицевой счет
+    инкапсулирован в понятие клиента.
+    """
     lfm = models.CharField(max_length=200)
     organization = models.ForeignKey(Organization)
+    amount = models.DecimalField(max_digits=8, decimal_places=2, default=1)
     def __str__(self):
         return self.lfm + "(" + self.organization.__str__() + ")"
 
 class RealEstate(models.Model):
+    """Объект недвижимости.
+
+    Объектом недвижимости может являться многоквартирный дом, жилой дом,
+    квартира, комната. При этом у модели есть связь, которая показывает,
+    например, какая именно комната принадлежит какой именно квартире.
+    """
     address = models.CharField(max_length=200)
     parent = models.ForeignKey('self', null=True, blank=True, default = None)
     def __str__(self):
         return self.address
 
 class Payment(models.Model):
+    """Платеж.
+
+    Платеж увеличивает сумму на лицевом счете клиента.
+    TODO: Текущая модель рабочая, но не актуальная.
+    """
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     client = models.ForeignKey(Client)
     real_estate = models.ForeignKey(RealEstate, null=True, blank=True, default = None)
+
+class ServiceClient(models.Model):
+    """Связь услуга-клиент.
+
+    Связь указывает на то, какая именно услуга потребляется определенным
+    клиентом.
+    """
+    client = models.ForeignKey(Client)
+    service_name = models.CharField(max_length=200)
+    pass
+
+class ColdWaterCounter(models.Model):
+    """Показания приборов учета холодной воды."""
+    value = models.IntegerField()
+    real_estate = models.ForeignKey(RealEstate)
+    date = models.DateField()
+
+class ColdWaterValue(models.Model):
+    """Вычисления объема потребления холодной воды."""
+    value = models.IntegerField()
+    real_estate = models.ForeignKey(RealEstate)
+    date = models.DateField()
