b59d0bf238924452e8d4e5d874cbf35fa0d20ad6
Alexander Makarevich <alexander.s.makarevich@gmail.com>
2014-11-07 12:30:00 +0600

    Перерасчет к ОДН

--- robot/algorithm.py
+++ robot/algorithm.py
@@ -36,7 +36,15 @@ def calculate_individual_cold_water_volume(real_estate, cold_water_norm, residen
     Как быть в случае, когда установлен счетчик с ненулевым показанием? Ведь
     тогда откуда брать показание, оно будет явно описано в данных, а не будет
     хардкода в алгоритме.
+    Возвращает объект в котором содержатся:
+    volume-объем индивидуального потребления, включая перерасчет
+    перерасчета_volume
     """
+    class IndividualColdWaterVolume:
+        def __init__(self, individual, recalculated=0):
+            self.individual = individual
+            self.recalculated = recalculated
+
     periods_with_counter = None
     setup_date = real_estate.cold_water_counter_setup_date
     if setup_date:
@@ -58,7 +66,7 @@ def calculate_individual_cold_water_volume(real_estate, cold_water_norm, residen
                 raise ForgottenInitialReadingError
             unconfirmed_reading_volumes = ColdWaterVolume.objects.filter(real_estate=real_estate, period__serial_number__in=range(last_period_reading.period.serial_number+1, next_to_last_period_reading.period.serial_number)).aggregate(Sum('volume'))['volume__sum'] or 0
             volume = last_period_reading.value - next_to_last_period_reading.value - unconfirmed_reading_volumes
-            return volume
+            return IndividualColdWaterVolume(volume, unconfirmed_reading_volumes)
 
         second_from_the_end_period_reading = ColdWaterReading.objects.filter(real_estate=real_estate, period=periods_with_counter[periods_with_counter.count()-2]).last()
         third_from_the_end_period_reading = ColdWaterReading.objects.filter(real_estate=real_estate, period=periods_with_counter[periods_with_counter.count()-3]).last()
@@ -66,20 +74,20 @@ def calculate_individual_cold_water_volume(real_estate, cold_water_norm, residen
         if was_reading_in_second_or_third_period_from_the_end:
             last_six_volumes_sum = ColdWaterVolume.objects.filter(real_estate=real_estate, period__serial_number__in=range(periods_with_counter.last().serial_number-6+1, periods_with_counter.last().serial_number+1)).aggregate(Sum('volume'))['volume__sum']
             average_volume = last_six_volumes_sum/6
-            return average_volume
+            return IndividualColdWaterVolume(average_volume)
 
     share = calculate_share_of_service_usage(real_estate, ServiceClient.COLD_WATER_SERVICE, Period.objects.all().last())
     if residential: #или счетчик меньше 6 периодов, или без счетчика
         #Формула № 4а
         volume = residents * cold_water_norm
-        return volume * share
+        return IndividualColdWaterVolume(volume * share)
 
     periods = Period.objects.order_by('start')
     start_period_index = periods.count()-6-1
     if start_period_index > 0:
         last_six_volumes_sum = ColdWaterVolume.objects.filter(real_estate=real_estate, period__serial_number__gte=periods[start_period_index].serial_number).aggregate(Sum('volume'))['volume__sum']
         average_volume = last_six_volumes_sum/6
-        return average_volume * share
+        return IndividualColdWaterVolume(average_volume * share)
 
     return None #TODO: нужен флаг -- manual
 
@@ -103,6 +111,7 @@ def write_off():
                     #{Начало расчета индивидуального потребления:
                     #{
                     cold_water_volume_clients_sum = 0
+                    recalculated_volume = 0
                     for real_estate in real_estates:
                         if real_estate.type != RealEstate.FLAT_TYPE or real_estate.type != RealEstate.SHARE_TYPE:
                             raise Exception #TODO: Продумать ошибку.
@@ -110,8 +119,9 @@ def write_off():
                         #вычисляем для квартир, комунальных квартир и блоков объем потребления услуги, сохраняем его.
                         cold_water_norm = ColdWaterNorm.objects.filter(residential=real_estate.residential, region=building.region).get()
                         volume = calculate_individual_cold_water_volume(real_estate, cold_water_norm, real_estate.residential, real_estate.residents)
-                        volume_model = ColdWaterVolume(period=periods.last(), real_estate=real_estate, volume=volume, date=datetime.date.today())
+                        volume_model = ColdWaterVolume(period=periods.last(), real_estate=real_estate, volume=volume.individual, date=datetime.date.today())
                         volume_model.save()
+                        recalculated_volume = recalculated_volume + volume.recalculated
 
                         if real_estate.type == RealEstate.SHARE_TYPE:
                             total_rooms_volume = 0 #TODO: если total_rooms_volume != volume, то разница объемов будет учтена и рассчитана в расчет ОДН.
@@ -175,7 +185,7 @@ def write_off():
                         not_residential_cold_water_volume = 0
                         cold_water_building_volume = residential_cold_water_volume + not_residential_cold_water_volume
 
-                    volume = cold_water_building_volume - cold_water_volume_clients_sum
+                    volume = cold_water_building_volume - cold_water_volume_clients_sum + recalculated_volume
                     if volume != 0:
                         for real_estate in real_estates:
                             if real_estate.type != RealEstate.SHARE_TYPE:
@@ -215,7 +225,7 @@ def write_off():
                 does_cold_water_counter_exist = False
                 if does_cold_water_counter_exist:
                     volume = calculate_individual_cold_water_volume(house, cold_water_norm, house.residential, house.residents)
-                    volume_model = ColdWaterVolume(period=periods.last(), real_estate=house, volume=volume, date=datetime.date.today())
+                    volume_model = ColdWaterVolume(period=periods.last(), real_estate=house, volume=volume.individual, date=datetime.date.today())
                     volume_model.save()
                 else:
                     volume = house.residents * cold_water_norm
