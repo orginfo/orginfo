218f61b21546242923016eefb2eb39e984f21879
Alexander Makarevich <alexander.s.makarevich@gmail.com>
2014-11-30 12:10:11 +0600

    Добавил кнопку "Выйти"

--- accounting/urls.py
+++ accounting/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounting/login.html'}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login/'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login/'}, name="logout"),
     url(r'^organization_details/$', views.organization_details, name='organization_details'),
     url(r'^$', views.index, name='index'),
     #url(r'^report/$', views.report, name='report'),
