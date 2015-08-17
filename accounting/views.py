15ab1ad63182ff6c36bd716b8d4d6c893e15a731
Alexander Makarevich <alexander.s.makarevich@gmail.com>
2015-08-17 10:15:09 +0600

    Убрал "#with".

--- accounting/views.py
+++ accounting/views.py
@@ -187,8 +187,8 @@ def report(request):
 
     context = {}
 
-    #with setlocale('ru_RU.UTF-8'):
-    context["calc_period_name"] = period.end.strftime("%B %Y")
+    with setlocale('ru_RU.UTF-8'):
+        context["calc_period_name"] = period.end.strftime("%B %Y")
     context["owner"] = get_owner(real_estate, period)
     context["client_address"] = RealEstate.get_full_address(real_estate)
     context["space"] = get_real_estate_space(real_estate)
