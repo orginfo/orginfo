bae4a98fb02ec8664b3bea0438559fe72c8d63dc
Alexander Makarevich <alexander.makarevich@eltex.loc>
2015-09-10 12:28:41 +0600

    Добавил третий тест test_one_change_three_periods.

--- accounting/tests.py
+++ accounting/tests.py
@@ -102,6 +102,7 @@ class HomeownershipHistoryTests(TestCase):
         response = self.client.get(reverse('accounting:homeownership_history'))
         self.assertJSONEqual(json.dumps(response.context['table']), json.dumps(expected_data), msg=None)
 
+    @mock.patch('datetime.date', FakeDate)
     def test_one_change_three_periods(self):
         """
         +----------------------------------------------------+--------------+
@@ -112,7 +113,61 @@ class HomeownershipHistoryTests(TestCase):
         | Лошади           |        |                   4                  |
         +------------------------------------------------------------------+
         """
-        self.assertEqual(False, True)
+        water_desc17 = WaterNormDescription(description="Лошади", direction_type=WaterNormDescription.AGRICULTURAL_ANIMALS)
+        water_desc17.save()
+        subjectRF = SubjectRF(name="Новосибирская область")
+        subjectRF.save()
+        municipal_area = MunicipalArea(name="Тогучинский район", subject_rf=subjectRF)
+        municipal_area.save()
+        union1 = MunicipalUnion(name="Кудельно-Ключевской сельсовет", municipal_area=municipal_area)
+        union1.save()
+        loc3 = Locality(name="Кудельный Ключ", type=Locality.HAMLET, subject_rf=subjectRF, municipal_area=municipal_area, municipal_union=union1)
+        loc3.save()
+        street4 = Street(name="Лесная", locality=loc3)
+        street4.save()
+        address = HouseAddress(index="633447", street=street4, house_number=20)
+        address.save()
+        real_estate = RealEstate(address=address, number="")
+        real_estate.save()
+        homeownership_history = HomeownershipHistory(real_estate=real_estate, water_description=water_desc17, count=4, start='2015-08-22')
+        homeownership_history.save()
+
+        #print(HomeownershipHistory.objects.all()[0].start)
+        expected_data = [
+            [{
+                "name": "Расчётный период"
+            }, {
+                "name": "Август 2015",
+                "length": 2
+            }, {
+                "name": "Сентябрь 2015"
+            }, {
+                "name": "Октябрь 2015"
+            }],
+            [{
+                "name": "Изм-но с"
+            }, {
+                "name": ""
+            }, {
+                "name": "←22.08"
+            }, {
+                "name": "←26.08"
+            }, {
+                "name": "←26.09"
+            }],
+            [{
+                "name": "Лошади"
+            }, {
+                "name": ""
+            }, {
+                "name": 4.0,
+                "length": 3
+            }]
+        ]
+        FakeDate.today = classmethod(lambda cls: date(2015, 10, 3))
+        self.client.login(username='nn', password='nn')
+        response = self.client.get(reverse('accounting:homeownership_history'))
+        self.assertJSONEqual(json.dumps(response.context['table']), json.dumps(expected_data), msg=None)
 
     def test_two_changes_three_periods(self):
         """
